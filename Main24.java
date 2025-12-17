e8b7b836fd43b1d0ad66632421f3d05b3bff6419
Dev Sathwik P <82132397+Sathwik3010@users.noreply.github.com>
2025-12-17 16:52:06 +0530

    LinkedHashSet

--- Main24.java
+++ Main24.java
@@ -1,10 +1,47 @@
 package com.codegnan.collections;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main24 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Set<Integer> set = new LinkedHashSet<>();
 
+		String line = sc.nextLine();
+
+		if (!line.trim().isEmpty()) {
+			String[] arr = line.split(" ");
+			for (String s : arr) {
+				set.add(Integer.parseInt(s));
+			}
+		}
+
+		int removeValue = sc.nextInt();
+
+		Iterator<Integer> it = set.iterator();
+		while (it.hasNext()) {
+			if (it.next() == removeValue) {
+				it.remove();
+				break;
+			}
+		}
+
+		Iterator<Integer> printIt = set.iterator();
+		StringBuilder sb = new StringBuilder();
+		while (printIt.hasNext()) {
+			sb.append(printIt.next());
+			if (printIt.hasNext()) {
+				sb.append(" ");
+			}
+
+		}
+
+		System.out.println(sb.toString());
+		sc.close();
 	}
 
 }
